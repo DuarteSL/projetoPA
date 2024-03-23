591093380a7e425e24f7a7a84a1925f8eb5681d0
DuarteSL <dslor@iscte-iul.pt>
2024-03-23 20:53:56 +0000

    Delete old models + visualization done

--- src/main/kotlin/Visualization.kt
+++ src/main/kotlin/Visualization.kt
@@ -1,31 +1,3 @@
-fun Document.toText(): String {
-    return buildString {
-        append("<?xml version=\"${getVersion()}\" encoding=\"${getEncoding()}\"?>\n")
-        append(getRoot().toText())
-    }
-}
-
-fun Entity.toText(): String {
-    var s = buildString {
-        if (getText().isEmpty() && getChildren().isEmpty()) {
-            append("${"\t".repeat(depth - 1)}<${getName()}${getAttributes().joinToString("") { it.toText() }}/>\n")
-        } else if (getText().isNotEmpty() && getChildren().isEmpty()) {
-            append("${"\t".repeat(depth - 1)}<${getName()}${getAttributes().joinToString("") { it.toText() }}>${getText()}</${getName()}>\n")
-        } else {
-            append("${"\t".repeat(depth - 1)}<${getName()}${getAttributes().joinToString("") { it.toText() }}>\n")
-            getChildren().forEach {
-                append(it.toText())
-            }
-            append("${"\t".repeat(depth - 1)}</${getName()}>\n")
-        }
-    }
-    return s
-}
-
-fun Attribute.toText(): String {
-    return " ${this.getName()}=\"${this.getValue()}\""
-}
-
 fun XMLDocument.toText(): String {
     return buildString {
         append("<?xml version=\"${getVersion()}\" encoding=\"${getEncoding()}\"?>\n")
